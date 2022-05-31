a66c667ae99ca25550fa37d748da1e128cff127f
Alexoat76 <3915@holbertonschool.com>
2022-05-31 18:47:36 -0500

    Fix styling :heavy_check_mark:

---
+++ api/v1/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# File: __init__.py
+# Main Authors: Alexa Orrico - Jennifer Huang
+# email(s): <210@holbertonschool.com>
+#           <133@holbertonschool.com>
+#
+# Secondary collaborator: Joann Vuong
+# email: <130@holbertonschool.com>
+#
+# Tertiary collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
